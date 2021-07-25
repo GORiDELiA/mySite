f9450f3960f690dce963d131eafe925157ba83fa
goridelia <tech.asano@gmail.com>
2021-07-25 23:31:29 +0900

    study 20210725

--- src/App.vue
+++ src/App.vue
@@ -31,7 +31,7 @@
   <div :style="[baseStyleObject, successStyleObject]"> Success Object</div>
   <div :style="[baseStyleObject, dangerStyleObject]"> Danger Object</div> -->
 
-  <h2 v-if="num === 0">The number is zero</h2>
+  <!-- <h2 v-if="num === 0">The number is zero</h2>
   <h2 v-else-if="num < 0">The number is negative</h2>
   <h2 v-else-if="num > 0">The number is positive</h2>
   <h2 v-else>The number is not number</h2>
@@ -41,10 +41,30 @@
     <h2>Codevolution</h2>
     <h2>Vue</h2>
   </template>
+
+  <h2 v-show="showElement">Using v-show</h2>
+  <h2 v-if="showElement">Using v-if</h2> -->
+
+  <!-- <h2 v-for="(name, index) in names" :key="name">{{ index }} {{ name }}</h2>
+  <h2 v-for="name in fullNames" :key="name.first">{{ name.first }} {{ name.last }}</h2>
+  <div v-for="actor in actors" :key="actor.name">
+    <h2>{{actor.name}}</h2>
+    <h3 v-for="movie in actor.movies" :key="movie">{{ movie }}</h3>
+  </div>
+  <h2 v-for="(value, key, index) in myInfo" :key="value">{{ index }} {{ key }} {{ value }}</h2>
+  
+  <template v-for="name in names" :key="name">
+    <h2>{{ name }} </h2>
+    <hr />
+  </template> -->
+
+  <template v-for="name in names" :key="name">
+    <h2 v-if="name === 'Bruce'">{{ name }}</h2>
+  </template>
+
 </template>
 
 <script>
-
 export default {
   name: 'App',
   data() {
@@ -81,8 +101,31 @@ export default {
       //   border: "1px solid darkred",
       // },
 
-      num: "Hi",
-      display: true,
+      // num: "Hi",
+      // display: true,
+      // showElement: false,
+
+      names: ["Bruce", "Clark", "Diana"],
+      fullNames: [
+        { first: "Bruce", last: "Wayne" },
+        { first: "Clark", last: "Kent" },
+        { first: "Princess", last: "Diana" },
+      ],
+      actors: [
+        {
+          name: "Christian Bale",
+          movies: ["Batman", "The Prestige"],
+        },
+        {
+          name: "Di Caprio",
+          movies: ["Titanic", "Inception"],
+        },
+      ],
+      myInfo: {
+        name: "Vishwas",
+        channel: "Codevolution",
+        cource: "Vue 3",
+      },
 
     }
   },
