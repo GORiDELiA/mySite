59c7789e5e20ea7fa87ed1a4576f11a789539878
goridelia <tech.asano@gmail.com>
2021-08-09 22:17:38 +0900

    Study codevolution 21-22

--- src/App.vue
+++ src/App.vue
@@ -184,6 +184,10 @@
 
   <h2>Fullname - {{ firstName}} {{lastName}} </h2>
   <h2>Fullname - {{ fullName }} </h2>
+  <h2>Total - {{ items.reduce((total, curr) => (total = total + curr.price), 0) }} </h2>
+  <button @click="items.push({id: 4, title: 'Keyboard', price: 50})">Add item</button>
+  <h2>Computed Total - {{ total }}</h2>
+
 </template>
 
 <script>
@@ -262,9 +266,26 @@ export default {
       //   yearsOfExperience: "",
       //   age: null,
       // },
-      // name: "Vishwas",
+      name: "Vishwas",
       firstName: 'Bruce',
       lastName: 'Wayne',
+      items: [
+        {
+          id: 1,
+          title: "TV",
+          price: 100,
+        },
+        {
+          id: 2,
+          title: "Phoen",
+          price: 200,
+        },
+        {
+          id: 3,
+          title: "Laptop",
+          price: 300,
+        },
+      ],
     }
   },
   methods: {
@@ -292,8 +313,12 @@ export default {
   },
   computed: {
     fullName() {
-      return `${this.firstName} ${this.lastName}`
+      // return `${this.firstName} ${this.lastName}`
+      return this.firstName + " "+this.lastName
     },
+    total() {
+      return this.items.reduce((total, curr) => (total = total + curr.price), 0) 
+    }
   }
 }
 </script>
@@ -303,7 +328,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
+  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
