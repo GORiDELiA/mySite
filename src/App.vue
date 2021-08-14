aa0095aebb8e2640cd1349010889e654cdb61c55
GORiDELiA <general.asano@gmail.com>
2021-08-14 15:00:51 +0900

    codevolution 26-27

--- src/App.vue
+++ src/App.vue
@@ -197,6 +197,13 @@
   <h2 v-for="item in expensiveItems" :key="item.id">
     {{ item.title }} {{ item.price }}
   </h2>
+
+  <h2>Volume Tracker (0-20)</h2>
+  <h3>Current Volume - {{ volume }}</h3>
+  <div>
+    <button @click="volume += 2" >Increase</button>
+    <button @click="volume -= 2" >Decrease</button>
+  </div>
 </template>
 
 <script>
@@ -296,6 +303,8 @@ export default {
         },
       ],
       country: '',
+      volume: 0,
+
     }
   },
   methods: {
@@ -336,7 +345,14 @@ export default {
     expensiveItems() {
       return this.items.filter( item => item.price > 100)
     },
-  }
+  },
+  watch: {
+    volume(newValue, oldValue) {
+      if(newValue > oldValue && newValue === 16) {
+        alert('Listening to a high volume for a long time may damage your hearing')
+      }
+    },
+  },
 }
 </script>
 
