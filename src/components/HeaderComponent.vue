30733ded1177a6424986bdb247f2c7092a746e70
GORiDELiA <general.asano@gmail.com>
2021-08-17 00:18:54 +0900

    remake study

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <h2>{{ pageTitle}}</h2>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            pageTitle: 'ASMRer header',
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
