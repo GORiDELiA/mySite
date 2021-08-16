175d81e9a3f95a4286bf0a7fff9d82e77cd73430
GORiDELiA <general.asano@gmail.com>
2021-08-17 00:08:46 +0900

    add Header Footer

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <h2>{{ pageTitle}}</h2>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            pageTitle: 'ASMRer footer',
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
